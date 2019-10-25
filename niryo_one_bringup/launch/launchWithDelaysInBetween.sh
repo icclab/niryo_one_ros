e8bbab8577630bd9dfc333af7ea073fb6a32da8a
Georgios Leandros Kyriazis <leankyr@gmail.com>
2019-10-25 11:03:58 +0000

    Managed to launch everything without problem

---
+++ niryo_one_bringup/launch/launchWithDelaysInBetween.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# dependencies: timed roslaunch
+# https://github.com/MoriKen254/timed_roslaunch
+
+rosrun timed_roslaunch timed_roslaunch.sh 0 niryo_one_bringup base_world_model.launch &
+
+rosrun timed_roslaunch timed_roslaunch.sh 20 niryo_one_bringup controllers.launch &
+
+rosrun timed_roslaunch timed_roslaunch.sh 40 niryo_one_bringup robot_commander.launch &
+
+rosrun timed_roslaunch timed_roslaunch.sh 60 niryo_one_bringup launch_rviz.launch &
+
+sleep 80
+
+read -n 1 -s -r -p "Press any key to continue"
+
+echo -n "killing gserver..."
+pkill -9 gzserver
+sleep 5
+echo -n "killing gzclient..."
+pkill -9 gzclient
+sleep 5
+echo -n "killing all nodels..."
+## THIS ONE LATER MIGHT BREAK THE CODE
+## SHOULD KILL THE NODES 1 by 1 EXPLICTLY BY NAME 
+rosnode kill -a
+sleep 5
+echo -n "killing rosmaster..."
+pkill -9 rosmaster
+echo -n "PRESS Ctrl-C TO TERMINATE THIS PROCESS"
+echo -n "Yatta!!!!!"
+
+
